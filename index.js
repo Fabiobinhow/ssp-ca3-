ce4d25af7d003f8d1c72ea6d502b4956eace6281
Fabio Teixeira Ramos <fabioteixeira0505@gmail.com>
2022-01-05 11:22:07 +0000

    add gitignore

--- index.js
+++ index.js
@@ -8,3 +8,46 @@ const   // we use the const and we dont need to change the value
         xml2js = require('xml2js'); //This module does XML at JSON conversion
 
          //I used the code used in class :22/11/2021
+
+const   router = express(), 
+         server = http.createServer(router);
+ 
+ 
+ router.use(express.static(path.resolve(__dirname,'views'))); //We serve static content from "views" folder
+ router.use(express.urlencoded({extended: true})); //We allow the data sent from the client to be encoded in a URL targeting our end point
+ router.use(express.json()); //We include support for JSON
+ 
+ 
+ router.get('/get/html', function(req, res) {
+ 
+     res.writeHead(200, {'Content-Type' : 'text/html'}); // this will tell to the broser for the content
+ 
+     let xml = fs.readFileSync('Motor.xml', 'utf8'),
+         xsl = fs.readFileSync('Motor.xsl', 'utf8');
+ 
+     console.log(xml);
+     console.log(xsl);
+ 
+     let doc = xmlParse(xml),
+         stylesheet = xmlParse(xsl);
+ 
+     console.log(doc);
+     console.log(stylesheet);
+ 
+     let result = xsltProcess(doc, stylesheet);
+ 
+     console.log(result);
+ 
+     res.end(result.toString());
+ 
+ });
+ 
+ 
+ server.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function() 
+ {
+     const addr = server.address();
+     console.log("Server listening at", addr.address + ":" + addr.port)
+ });
+ 
+    
+
